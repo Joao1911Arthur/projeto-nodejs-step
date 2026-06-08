91bbf9541c114bd3fa2a504c0cbbc87a20c8a142
Joao1911Arthur <arthur1911br@gmail.com>
2026-06-08 19:57:39 -0300

    calma

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+
+require('dotenv').config();
 
 const app = require('./src/app');
 
@@ -14,3 +15,4 @@ app.listen(process.env.PORT, () => {
     'Servidor rodando'
   );
 });
+            
--- src/database/database.js
+++ src/database/database.js
@@ -1,17 +1,21 @@
+
 const mongoose = require('mongoose');
 
 const conectarBanco = async () => {
-    try {
-        await mongoose.connect(
-            process.env.MONGO_URL
-        );
 
-        console.log('MongoDB conectado');
+  try {
 
-    } catch (error) {
+    await mongoose.connect(
+      process.env.MONGO_URL
+    );
 
-        console.log(error);
-    }
+    console.log('MongoDB conectado');
+
+  } catch (error) {
+
+    console.log(error);
+  }
 };
 
 module.exports = conectarBanco;
+          
