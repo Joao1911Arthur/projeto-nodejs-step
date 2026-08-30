ec38cd8425485bfc6a9038423d9d671dfec2244b
Joao1911Arthur <arthur1911br@gmail.com>
2026-08-30 10:35:24 -0300

    atualizando URL img

--- seed.js
+++ seed.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
  
-const conectar = require("./src/database/connect");
-const Produto = require("./src/models/Produto");
-const Usuario = require("./src/models/Usuario");
+const conectar = require("./src/database/database");
+const Produto = require("./src/models/Produtos");
+const Usuario = require("./src/models/Usuarios");
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
  
 async function seed() {
-  await conectar();
+
+await conectar.conectar();
   console.log("MongoDB conectado — populando dados...");
  
   await Produto.deleteMany({});
@@ -19,7 +20,7 @@ async function seed() {
       preco: 109.95,
       descricao: "Sua mochila perfeita para o dia a dia e viagens.",
       categoria: "masculino",
-      imagem: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      imagem: "https://acdn-us.mitiendanube.com/stores/001/374/022/products/7392158958184_ss18_a_foldsack_no1_21-fe1ce956680dbda28617682375381265-1024-1024.webp?w=1920",
       avaliacao: { nota: 3.9, quantidade: 120 },
     },
     {
@@ -28,7 +29,7 @@ async function seed() {
       descricao: "Camiseta de algodão com estilo casual.",
       categoria: "masculino",
       imagem:
-        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRocrFhqeEnf9-uT_BVXNq5BXd0f91oX9C3LucDjW2MladBvvzWnkJadhLYtz41Tvd8bTy0Ii3U--zF1yO9XOaYsnDNeXfBtawPK9ZPZ1CFQlXW9QESFvYIfqXnIbk9mJADDCqXMg&usqp=CAc",
       avaliacao: { nota: 4.1, quantidade: 259 },
     },
     {
@@ -36,7 +37,7 @@ async function seed() {
       preco: 55.99,
       descricao: "Jaqueta quente e estilosa para o inverno.",
       categoria: "masculino",
-      imagem: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+      imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTozUwGoFvPlFOFDgx9KqTaJVcm_2zaPnLKLQW0ox4_mA&s=10",
       avaliacao: { nota: 4.7, quantidade: 500 },
     },
     {
@@ -44,7 +45,7 @@ async function seed() {
       preco: 29.95,
       descricao: "Vestido leve e confortável para o verão.",
       categoria: "feminino",
-      imagem: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+      imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzlzeBF-ZJ8F__ufwLoRKmfLQf8D5ROTjivHXvrjSpnw&s=10",
       avaliacao: { nota: 3.8, quantidade: 240 },
     },
     {
@@ -52,7 +53,7 @@ async function seed() {
       preco: 89.99,
       descricao: "Bolsa elegante em couro legítimo.",
       categoria: "feminino",
-      imagem: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+      imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5j7f_IBRCd3ZdRM8bXoGU8uv-NgalaWASEen3PZiroQ&s=10",
       avaliacao: { nota: 4.2, quantidade: 180 },
     },
     {
@@ -60,7 +61,7 @@ async function seed() {
       preco: 49.99,
       descricao: "Relógio resistente à água com cronômetro.",
       categoria: "eletronicos",
-      imagem: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+      imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrTqKohgOogRQdCuCKIA0mtyx1MpQgfxmjM84UEFxmRg&s=10",
       avaliacao: { nota: 4.5, quantidade: 320 },
     },
     {
@@ -68,7 +69,7 @@ async function seed() {
       preco: 79.99,
       descricao: "Fone sem fio com cancelamento de ruído.",
       categoria: "eletronicos",
-      imagem: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+      imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT68Xwl_kw_bWsFUD-3N2ETFDOf2b9qAj-EHhgd_WO3fw&s=10",
       avaliacao: { nota: 4.3, quantidade: 410 },
     },
     {
@@ -77,7 +78,7 @@ async function seed() {
       descricao: "Anel elegante em prata 925.",
       categoria: "joias",
       imagem:
-        "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEpXp21JFMaUGZRMk9GBCK0-IpL9V7CBrLDLUjWzK-DQ&s=10",
       avaliacao: { nota: 3.9, quantidade: 70 },
     },
   ]);
